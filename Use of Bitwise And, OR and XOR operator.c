62004d5649944dc62ffa06c2f3c6cd37f5ba6f80
Abhishek Kumar Singh <83462616+Abhishek5212@users.noreply.github.com>
2021-07-16 22:46:45 +0530

    Create Use of Bitwise And, OR and XOR operator.c

---
+++ Use of Bitwise And, OR and XOR operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main() 
+{ 
+    unsigned char a = 5, b = 9; 
+    printf("a = %d, b = %d\n", a, b); 
+    printf("a&b = %d\n", a & b); 
+    printf("a|b = %d\n", a | b); 
+    printf("a^b = %d\n", a ^ b); 
+    return 0; 
+} 

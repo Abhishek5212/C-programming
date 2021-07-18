f6c769d922e507d60ff2fce3542c4d29ee9f68f4
Abhishek Kumar Singh <83462616+Abhishek5212@users.noreply.github.com>
2021-07-18 18:11:11 +0530

    Create Call by value function.c

---
+++ Call by value function.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+/* function declaration */
+void swap(int x, int y);
+ 
+int main () {
+
+   /* local variable definition */
+   int m = 50;
+   int n = 670;
+ 
+   printf("Before swap, value of m : %d\n", n );
+   printf("Before swap, value of m : %d\n", n );
+ 
+   /* calling a function to swap the values */
+   swap(m, n);
+ 
+   printf("After swap, value of m : %d\n", m );
+   printf("After swap, value of n : %d\n", n );
+ 
+   return 0;
+}
+void swap(int x, int y) {
+
+   int temp;
+
+   temp = x; /* save the value of x */
+   x = y;    /* put y into x */
+   y = temp; /* put temp into y */
+  
+   return;
+}

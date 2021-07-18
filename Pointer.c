faabda9c5a915fbe8cbfb7baede07a25efff4dc9
Abhishek Kumar Singh <83462616+Abhishek5212@users.noreply.github.com>
2021-07-18 18:16:18 +0530

    Update Pointer.c

--- Pointer.c
+++ Pointer.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main () 
+int main ()
 {
-   int  var1;
-   char var2[10];
+   int  a;
+   char b[10];
 
-   printf("Address of var1 variable: %x\n", &var1  );
-   printf("Address of var2 variable: %x\n", &var2  );
+   printf("Address of a variable: %x\n", &a );
+   printf("Address of b variable: %x\n", &b );
    return 0;
 }

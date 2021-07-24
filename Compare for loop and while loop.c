2222bd00f0f2a94a9a9a9e51bc52b0926498981f
Abhishek Kumar Singh <83462616+Abhishek5212@users.noreply.github.com>
2021-07-24 20:41:30 +0530

    Update Compare for loop and while loop.c

--- Compare for loop and while loop.c
+++ Compare for loop and while loop.c
@@ -3,13 +3,13 @@ int main()
 {
  int i;
  printf("Abhishek©");
- printf("In for loop");
+ printf("for loop");
  for(i=0; i<=6; i++)
  {
    printf("The value is %d \n",i);
  }
  
- printf("In while loop\n");
+ printf("while loop\n");
  int j=0;
  while(j<=6)
  {

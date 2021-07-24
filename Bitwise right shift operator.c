373f59a5ca4af084a9477921d5b3edb9594c0f7c
Abhishek Kumar Singh <83462616+Abhishek5212@users.noreply.github.com>
2021-07-24 20:44:36 +0530

    Create Bitwise right shift operator.c

---
+++ Bitwise right shift operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int i = 6; 
+   printf("Abhishek ©") 
+   printf("pow(2, %d) = %d\n", i, 1 << i);
+   i = 3; 
+   printf("pow(2, %d) = %d\n", i, 1 << i);
+   return 0;
+}
